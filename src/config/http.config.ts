e12093a1e4f15a05af7b412df455a2c56d20a355
taorui <792209917@qq.com>
2022-01-04 11:28:26 +0800

    update

--- src/config/http.config.ts
+++ src/config/http.config.ts
@@ -1,8 +1,7 @@
 // api基础域名
 let mainDomainName = '/devUrl';
-let frontEndUrl = 'http://localhost:8082'
 if (import.meta.env.MODE === 'test') {
-     mainDomainName = "http://172.31.40.171:3000";
+    mainDomainName = "http://172.31.40.171:3000";
 } 
 if (import.meta.env.MODE === 'production') {
     mainDomainName = "";
